e6a8ab49c3f2c3352036c4f74e81b0eef2465584
Rohan Antony <rohan271995@gmail.com>
2022-03-12 17:41:39 +0100

    Adding class based solution

--- README.md
+++ README.md
@@ -4,5 +4,6 @@
 3. Activate virtual environment `.\Scripts\activate`
 4. Run `pip install -r requirements.txt` to setup project
 5. Rename `config-template.json` to `config.json` with actual values set in placeholder
+6. Install spacy libraries by running `python -m spacy download en_core_web_sm`
 
 #
---
+++ src/main.py
@@ -0,0 +1,70 @@
+import pandas as pd
+import nltk
+
+import re
+import string
+
+import spacy
+import transformers
+import torch 
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+
+class TweetAnalytics:
+
+  def __init__(self):
+    try:
+      nltk.download('wordnet')
+      nltk.download('stopwords')
+      self.nlp = spacy.load("en_core_web_sm")
+    except IOError:
+      print('Error fetching spacy module or nltk')
+    self.ps = nltk.PorterStemmer()
+    self.stopwords = nltk.corpus.stopwords.words('english')
+    self.tokenizer = transformers.DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
+    self.model = transformers.DistilBertModel.from_pretrained('distilbert-base-uncased')
+    print("initialization")
+
+  def _clean_tweet(self, tweet):
+    for t in self.nlp(tweet):
+      if t.like_url:
+        pass
+      else:
+        text = "".join([t for t in tweet if t not in string.punctuation])
+        token = re.split("\W+", text)
+        final = [word for word in token if word not in self.stopwords]
+        stem_text = [self.ps.stem(word) for word in final]
+    return stem_text
+
+  def read_and_fetch_data(self, path_to_csv):
+    self.combined_df = pd.read_csv(path_to_csv,delimiter= ',')
+    self.combined_df['tweet_orig'] = self.combined_df['tweet']
+    self.combined_df['tweet'] = self.combined_df['tweet'].apply(lambda x: self._clean_tweet(x))
+
+  def setup_data_for_ml(self):
+    self.text_df = self.combined_df['tweet']
+    target_df = self.combined_df['sentiment']
+    tokenized_data = self.text_df.apply(lambda tweet : self.tokenizer.encode(tweet, max_length= 30))
+    input_text = pad_sequences(tokenized_data, maxlen=30, padding="post")
+    attention_mask = np.where(input_text != 0 ,1,0)
+    input_data_tensor = torch.tensor(input_text)
+    input_mask_tensor = torch.tensor(attention_mask) 
+    with torch.no_grad():
+      embedded_matrix = self.model(input_data_tensor, attention_mask = input_mask_tensor)
+    train_df = embedded_matrix.last_hidden_state[:,0,:].numpy()
+    target_df = target_df.to_numpy()
+    self.train_X, self.valid_X, self.train_y, self.valid_y = train_test_split(train_df, target_df, test_size = 0.2, random_state=15)
+
+  def train_with_random_forest_classifier(self):
+    clf = RandomForestClassifier(max_depth=2, random_state=0)
+    clf.fit(self.train_X, self.train_y)
+    
+
+if __name__ =="__main__":
+  tweet_analytics = TweetAnalytics()
+  tweet_analytics.read_and_fetch_data('./data/tweets_1000.csv')
+  tweet_analytics.setup_data_for_ml()
+  tweet_analytics.train_with_random_forest_classifier()
