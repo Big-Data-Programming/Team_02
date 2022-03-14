031c12bc35043aa868d20d686c6c5c9707c36229
Rohan Antony <rohan271995@gmail.com>
2022-03-14 18:50:45 +0100

    Adding mongodb changes and multiple model predictions

--- src/main.py
+++ src/main.py
@@ -10,15 +10,26 @@ import torch
 import mlflow
 import nltk
 import tweepy
+import hashlib
+import requests
 
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
+from pymongo import MongoClient
 
 class TweetAnalytics:
 
   def __init__(self):
+    client = MongoClient(host='localhost', port=27019, username='root', password='pass')
+    db = client.commodity
+    self.data = db.data
     with open('./config.json') as config_file:
       config = json.load(config_file)
     mlflow.set_tracking_uri(config['tracking_uri'])
@@ -38,6 +49,7 @@ class TweetAnalytics:
     self.api = tweepy.API(auth)
 
   def _clean_tweet(self, tweet):
+    stem_text = ''
     for t in self.nlp(tweet):
       if t.like_url:
         pass
@@ -49,10 +61,18 @@ class TweetAnalytics:
     return stem_text
 
   def read_and_fetch_data(self, path_to_csv):
-    combined_df = pd.read_csv(path_to_csv,delimiter= ',')
-    combined_df['tweet_orig'] = combined_df['tweet']
-    self.text_df = combined_df['tweet']
-    self.target_df = combined_df['sentiment']
+    data = pd.read_csv(path_to_csv,delimiter= ',', encoding='latin-1', header= None)
+    data.columns = ['sentiment', 'id', 'date','from','user','tweet']
+    data = data.drop(['id','date','from','user'] ,axis =1)
+    self.total = 2000
+    new_data_po = data[data['sentiment']==4].sample(int(self.total / 2))
+    new_data_ne = data[data['sentiment']==0].sample(int(self.total / 2))
+    data = pd.concat([new_data_ne,new_data_po])
+    self.filename = hashlib.md5(data.to_csv().encode('utf-8')).hexdigest() + '.csv'
+    self.filepath = './data/' + self.filename
+    data.to_csv(self.filepath)
+    self.text_df = data['tweet']
+    self.target_df = data['sentiment']
 
   def setup_data_for_ml(self, train = False):
     self.text_df = self.text_df.apply(lambda x: self._clean_tweet(x))
@@ -73,11 +93,42 @@ class TweetAnalytics:
     clf.fit(self.train_X, self.train_y)
     return clf
 
+  def train_with_logistic_regression(self):
+    clf = LogisticRegression()
+    clf.fit(self.train_X, self.train_y)
+    return clf  
+
+  def train_with_svc(self):
+    clf = SVC()
+    clf.fit(self.train_X, self.train_y)
+    return clf
+
+  def train_with_knc(self):
+    clf = KNeighborsClassifier(n_neighbors=5)
+    clf.fit(self.train_X, self.train_y)
+    return clf
+
+  def train_with_nbc(self):
+    clf = GaussianNB()
+    clf.fit(self.train_X, self.train_y)
+    return clf
+
   def train_with_mlflow(self, model_function, model_name):
     with mlflow.start_run() as run: 
       mlflow.sklearn.autolog()
       model = model_function()
+      predicted = model.predict(self.valid_X)
+      cm = confusion_matrix(predicted, self.valid_y)
+      tn, fp, fn, tp = cm.ravel()
       mlflow.sklearn.log_model(model,"model", registered_model_name=model_name)
+      mlflow.log_metric("tn", tn)
+      mlflow.log_metric('fp', fp)
+      mlflow.log_metric('fn', fn)
+      mlflow.log_metric('tp', tp)
+      mlflow.log_metric('validation precision score', tp / (tp + fp))
+      mlflow.log_metric('validation recall score', tp / (tp + fn))
+      mlflow.log_metric('data size', self.total)
+      mlflow.log_param('filepath', self.filepath)
   
   def fetch_twitter_data(self, keyword):
     tweets = []
@@ -86,23 +137,64 @@ class TweetAnalytics:
     self.text_df = pd.DataFrame(tweets, columns=['tweet'])
     self.text_df = self.text_df['tweet']
 
-  def test_with_model(self, model_name, model_stage):
-    model = mlflow.pyfunc.load_model(
-        model_uri =f'models:/{model_name}/{model_stage}'
-    )
-    print(model.predict(self.train_df))
+  def test_with_models(self, model_names, model_stage):
+    models = []
+    for model_name in model_names:
+      model = mlflow.pyfunc.load_model(
+          model_uri =f'models:/{model_name}/{model_stage}'
+      )
+      models.append({
+        model_name: model_name,
+        model: model 
+      })
+    data = {}
+    for index in range(0, len(self.train_df)):
+      tweet = self.text_df[index]
+      for model in models:      
+        result = model.model.predict(self.train_df[index])
+        name = model.model_name
+        data[name] = result
+      data['tweet'] = tweet
+      self.data.insert_one(data)
+    # self.data
 
 
 if __name__ =="__main__":
   tweet_analytics = TweetAnalytics()
-  model_name = 'Random Forest Classifier'
   model_stage = 'None'
-  tweet_analytics.read_and_fetch_data('./data/tweets_1000.csv')
+  tweet_analytics.read_and_fetch_data('./tweets_1mil.csv')
   tweet_analytics.setup_data_for_ml(train=True)
+  rfc_model_name = 'Random_Forest_Classifier'
   tweet_analytics.train_with_mlflow(
     tweet_analytics.train_with_random_forest_classifier,
-    model_name
+    rfc_model_name
+  )
+  lr_model_name = 'Logistic_Regression'
+  tweet_analytics.train_with_mlflow(
+    tweet_analytics.train_with_logistic_regression,
+    lr_model_name
+  )
+  svc_model_name = 'Support_Vector_Classifier'
+  tweet_analytics.train_with_mlflow(
+    tweet_analytics.train_with_svc,
+    svc_model_name
+  )
+  knc_model_name = 'K_Neighbours_Classifier'
+  tweet_analytics.train_with_mlflow(
+    tweet_analytics.train_with_knc,
+    knc_model_name
+  )
+  nbc_model_name = 'Naive_Bayes_Classifier'
+  tweet_analytics.train_with_mlflow(
+    tweet_analytics.train_with_nbc,
+    nbc_model_name
   )
   tweet_analytics.fetch_twitter_data('oil')
   tweet_analytics.setup_data_for_ml()
-  tweet_analytics.test_with_model(model_name, model_stage)
+  tweet_analytics.test_with_models([
+    rfc_model_name,
+    lr_model_name,
+    svc_model_name,
+    knc_model_name,
+    nbc_model_name
+  ], model_stage)
